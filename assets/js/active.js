20478ad53bfcf36cc972c0330874e9bd60f57a06
Khactiem1 <nguyenkhactiem2k1@gmail.com>
2021-04-30 14:48:29 +0700

    fix

--- assets/js/active.js
+++ assets/js/active.js
@@ -40,20 +40,6 @@ document.addEventListener("DOMContentLoaded",function(){
                 }
             }
         }
-        
-        const view__pass = $$('.PasswordField_iconButton__1LFBr');
-        const element__parent = view__pass[0].parentElement.querySelector('.auth-form__input');;
-        view__pass[0].onclick = function(){
-            view__pass[0].classList.remove('view__active');
-            view__pass[1].classList.add('view__active');
-            element__parent.type = "text";
-        }
-        view__pass[1].onclick = function(){
-            view__pass[1].classList.remove('view__active');
-            view__pass[0].classList.add('view__active');
-            element__parent.type = "password";
-        }
-        
 
 
         /* slide navbar mobile */
