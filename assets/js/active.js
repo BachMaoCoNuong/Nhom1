d0f5ca519ce2a74b640ab30f0d34c107f25a6137
KhacTiem1 <nguyenkhactiem2k1@gmail.com>
2021-06-04 12:12:32 +0700

    fix lại

--- assets/js/active.js
+++ assets/js/active.js
@@ -112,13 +112,7 @@ document.addEventListener("DOMContentLoaded",function(){
     /* Chat */
     (function(){
         /* Chat fb */
-        const ChatFB = $('circle');
-        ChatFB.setAttribute("fill", "#a50064");
-
-
-
-
-        /* const messenger = $('.icon__chat');
+        const messenger = $('.icon__chat');
         const chat = $('.chat__container');
         const close = $('.chat__close');
         var today = new Date();
@@ -171,6 +165,6 @@ document.addEventListener("DOMContentLoaded",function(){
         }
         chatWindow = $('.chat__user'); 
         let xH = chatWindow.scrollHeight; 
-        chatWindow.scrollTo(0, xH); */
+        chatWindow.scrollTo(0, xH);
     })()
 },false)
